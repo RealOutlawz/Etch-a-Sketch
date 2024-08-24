180d50d22237efbe863edfd2c3c1042ecea2b9b1
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2024-08-24 16:35:19 -0500

    Made the boxes into a grid

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const boxContainer = document.getElementById("container");
 function makeBox() {
   const box = document.createElement("div");
-  box.style.width = "50px";
-  box.style.height = "50px";
+  box.style.width = "100px";
+  box.style.height = "100px";
   box.style.backgroundColor = "gray";
   box.style.margin = "0px";
   return box;
