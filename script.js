66fb621b1103af03678f6546497045b6f3f9ffc2
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2024-08-24 17:38:44 -0500

    Added a button with the purpose to reset the grid and create a grid based on user input

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 const boxContainer = document.getElementById("container");
+const box = document.createElement("div");
+const newSquare = document.getElementById("newSquare");
+
+//Creates a box with event listeners to change the color when the user hover over them
 function makeBox() {
   const box = document.createElement("div");
   box.style.width = "100px";
   box.style.height = "100px";
   box.style.backgroundColor = "gray";
   box.style.margin = "0px";
+
+  box.addEventListener("mouseenter", () => {
+    box.style.backgroundColor = "black";
+  });
+
+  box.addEventListener("mouseleave", () => {
+    box.style.backgroundColor = "gray";
+  });
+
   return box;
 }
 
+//Creates 16 boxes using the makeBox function
 for (let i = 0; i < 16; i++) {
   freshBox = makeBox();
   boxContainer.appendChild(freshBox);
 }
+
+newSquare.addEventListener("click", () => {
+  let amountOfSquares = prompt("Please amount of squares you want per side");
+  while (boxContainer.firstChild) {
+    boxContainer.removeChild(boxContainer.firstChild);
+  }
+  for (let i = 0; i < amountOfSquares; i++) {
+    freshBox = makeBox();
+    boxContainer.appendChild(freshBox);
+  }
+});
